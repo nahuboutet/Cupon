3673d4e034b850231f4369cd32241edf9e589d5f
nahuboutet <nahuboutet@gmail.com>
2017-12-20 12:28:55 -0300

    Bundles generados

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,10 +19,13 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Cupon\OfertaBundle\OfertaBundle(),
+            new Cupon\CiudadBundle\CiudadBundle(),
+            new Cupon\TiendaBundle\TiendaBundle(),
+            new Cupon\UsuarioBundle\UsuarioBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
