31c441d0cbdda4e2cd5d47e68442d7643a8cd5c5
nahuboutet <nahuboutet@gmail.com>
2017-12-22 11:19:46 -0300

    Entidades creadas

--- src/Cupon/CiudadBundle/Entity/Ciudad.php
+++ src/Cupon/CiudadBundle/Entity/Ciudad.php
@@ -10,12 +10,43 @@ use Doctrine\ORM\Mapping as ORM;
   */
 
 class Ciudad {
-    /** @ORM\Column(type="integer") */
+    /**
+     *  @ORM\Id
+     *  @ORM\Column(type="integer") 
+     *  @ORM\GeneratedValue 
+     */
     protected $id;
     
     /** @ORM\Column(type="string", length=100) */
     protected $nombre;
     
     /** @ORM\Column(type="string", length=100) */
-    protected $slug;       
+    protected $slug;   
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre=$nombre;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+     public function setSlug($slug)
+    {
+        $this->slug=$slug;
+    }
+
+    public function getSlug(){
+        return $this->slug;
+    }
+    
+    public function __toString() {
+        return $this->getNombre();
+    }
 }
